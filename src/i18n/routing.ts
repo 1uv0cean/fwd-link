a90b506eb9200f1bd100729b824c6d8f213501a6
1uv0cean <songhwee1@naver.com>
2025-12-07 15:12:09 +0900

    [fix] Resolve routing issue

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,7 +4,7 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: LOCALES,
   defaultLocale: DEFAULT_LOCALE,
-  localePrefix: "as-needed",
+  localePrefix: "always",
 });
 
 export type { Locale };
